93ef11daf9ec92202a875502742323d45557158a
Nandasb3 <nandagopal.chandragam5@gmail.com>
2026-09-25 01:07:40 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -39,6 +40,7 @@ export default function RootLayout({
         <MobileNav />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
